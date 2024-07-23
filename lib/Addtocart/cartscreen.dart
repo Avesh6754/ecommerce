545648733ba2b5c8b5df28df3a0dd83d5a10b5b4
Avesh6754 <Aweshprajapati150@gmail.com>
2024-07-24 03:48:40 +0530

    24-07-2024

--- lib/Addtocart/cartscreen.dart
+++ lib/Addtocart/cartscreen.dart
@@ -1,7 +1,9 @@
+import 'package:ecommerce/home/component/home_udf.dart';
 import 'package:ecommerce/home/homepage.dart';
 import 'package:flutter/material.dart';
 
 import '../component/global.dart';
+import 'component/add_to_cart_udf.dart';
 
 class Cartscreen extends StatefulWidget {
   const Cartscreen({super.key});
@@ -14,74 +16,71 @@ class _CartscreenState extends State<Cartscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(15, 45, 15, 0),
-              child: Row(
-                children: [
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      },
-                      child: Icon(Icons.arrow_back_ios_new_outlined)),
-                  SizedBox(
-                    width: 80,
-                  ),
-                  Text(
-                    'Shopping Cart',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            lineudf(),
-            SizedBox(
-              height: 15,
-            ),
-            Column(
-              children: [
-                ...List.generate(
-                    Cartlist.length,
-                    (index) => Cartudf(
-                          Cartlist[index]['name'],
-                          Cartlist[index]['price'],
-                          Cartlist[index]['image'],
-                          index,
-                        )),
-              ],
-            ),
-            SizedBox(height: 50,),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-
-              ],
-            )
-          ],
+          child: Column(children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(15, 45, 15, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pushReplacementNamed('/home');
+                  },
+                  child: Icon(Icons.arrow_back_ios_new_outlined)),
+              Spacer(),
+             Home_Text(temp: 'Shopping Cart', number: 18, color: Colors.black),
+              Spacer(),
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: GestureDetector(onTap: (){
-        Navigator.of(context).pushNamed('/Check');
-      },
+        SizedBox(
+          height: 15,
+        ),
+        lineudf(),
+        SizedBox(
+          height: 2,
+        ),
+        Column(
+          children: [
+            ...List.generate(
+                Cartlist.length,
+                    (index) => Cartudf(
+                  Cartlist[index]['name'],
+                  Cartlist[index]['price'],
+                  Cartlist[index]['image'],
+                  index,
+                )),
+          ],
+        )
+      ])),
+      bottomNavigationBar: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pushNamed('/Check');
+        },
         child: Container(
           margin: EdgeInsets.fromLTRB(15, 0, 15, 15),
           height: 60,
           width: double.infinity,
           decoration: BoxDecoration(color: Colors.red),
           alignment: Alignment.center,
-          child: Text('Checkout',style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white,letterSpacing: 1,fontSize: 15),),
+          child: Text(
+            'Checkout',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                letterSpacing: 1,
+                fontSize: 15),
+          ),
+        ),
       ),
-    ),
-
     );
   }
 
-  Stack Cartudf(String name, int price, String img, int index) {
+
+
+  Widget Cartudf(String name, int price, String img, int index) {
     return Stack(
       children: [
         Container(
@@ -90,36 +89,31 @@ class _CartscreenState extends State<Cartscreen> {
           width: double.infinity,
           decoration: BoxDecoration(
               color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
               border: Border.all(color: Colors.grey, width: 0.2)),
           // alignment: Alignment.topRight,
           child: Padding(
-            padding: const EdgeInsets.only(left: 0),
+            padding: const EdgeInsets.only(left: 90),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                    margin: EdgeInsets.only(left: 90),
+                    margin: EdgeInsets.only(left: 20),
                     height: 30,
                     width: 90,
                     child: Image(
                         image:
-                            AssetImage('assets/image/2-removebg-preview.png'))),
-                Padding(
-                  padding: const EdgeInsets.only(left: 120),
-                  child: Text(
-                    '${name}',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
+                        AssetImage('assets/image/2-removebg-preview.png'))),
+                Text(
+                  '    ${name}',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 67),
-                  child: Text(
-                    '\$${price}',
-                    style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.red),
-                  ),
+                Text(
+                  '\$${price}',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
                 ),
                 SizedBox(
                   height: 50,
@@ -168,10 +162,11 @@ class _CartscreenState extends State<Cartscreen> {
           ),
         ),
         Container(
-          height: 197,
-          margin: EdgeInsets.fromLTRB(16, 17, 15, 0),
+          height: 199,
+          margin: EdgeInsets.fromLTRB(16, 15, 15, 0),
           width: 149,
           decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(15),bottomLeft: Radius.circular(15)),
               color: Colors.grey.shade100,
               image: DecorationImage(
                 image: AssetImage('$img'),
---
+++ lib/Addtocart/component/add_to_cart_udf.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+import '../../component/global.dart';
+
+
