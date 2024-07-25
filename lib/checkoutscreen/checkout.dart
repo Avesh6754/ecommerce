06dd4ec4bce1f71e5025a5717ca5a3e5fd65b7d0
Avesh6754 <Aweshprajapati150@gmail.com>
2024-07-26 00:12:00 +0530

    26-07-2024

--- lib/checkoutscreen/checkout.dart
+++ lib/checkoutscreen/checkout.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce/home/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce/Addtocart/cartscreen.dart';
 
@@ -52,7 +53,11 @@ class _CheckoutState extends State<Checkout> {
                           Cartlist[index]['name'],
                           Cartlist[index]['price'],
                           Cartlist[index]['image'],
-                          index,
+
+
+
+                          index
+
                         ))
               ],
             )
@@ -62,4 +67,121 @@ class _CheckoutState extends State<Checkout> {
       bottomNavigationBar: checkoutamount(context),
     );
   }
+
+
+Widget Cartudf(String name, double price, String img,int index) {
+  return Stack(
+    children: [
+      Container(
+        height: 200,
+        margin: EdgeInsets.fromLTRB(15, 15, 15, 0),
+        width: double.infinity,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(color: Colors.grey, width: 0.2)),
+        // alignment: Alignment.topRight,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 90),
+          child: Center(
+            child: Column(
+
+              children: [
+                Container(
+                    margin: EdgeInsets.only(left: 20),
+                    height: 30,
+                    width: 90,
+                    child: Image(
+                        image:
+                        AssetImage('assets/image/2-removebg-preview.png'))),
+                Text(
+                  '     ${name}',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  '    \$${price}',
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.red),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: EdgeInsets.only(right: 70),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.grey.shade400,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(Icons.remove),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        '1',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          color: Colors.red,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      Container(
+        height: 199,
+        margin: EdgeInsets.fromLTRB(16, 15, 15, 0),
+        width: 149,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                bottomLeft: Radius.circular(15)),
+            color: Colors.grey.shade100,
+            image: DecorationImage(
+              image: AssetImage('$img'),
+            )),
+        alignment: Alignment.topRight,
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: GestureDetector(
+            onTap: () {
+              setState(() {
+                Cartlist.removeAt(index);
+              });
+            },
+            child: Icon(
+              Icons.delete,
+              color: Colors.red,
+            ),
+          ),
+        ),
+      ),
+    ],
+  );
 }
+}
+var selecteddele=0;
